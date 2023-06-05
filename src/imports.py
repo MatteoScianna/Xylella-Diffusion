104775509725e0c9249acc40a8f54d2c620dcea4
MatteoScianna <107430131+MatteoScianna@users.noreply.github.com>
2023-06-05 10:26:44 +0200

    Update imports.py

---
+++ src/imports.py
@@ -0,0 +1,14 @@
+import pygeos
+import geopandas as gpd
+import pandas as pd
+import requests
+import matplotlib.pyplot as plt #to avoid the warning message by plotting the geometries
+import warnings
+warnings.simplefilter("ignore")
+import bs4 
+import fiona
+import folium
+import mapclassify
+import numpy as np 
+import networkx as nx
+import random as rand
